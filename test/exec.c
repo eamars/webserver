2d9369a7a4b49cabd8f4391e9bcaee5a536cc4db
Ran Bao <worksev@gmail.com>
2015-11-04 00:27:09 +1300

    fix header issue on linux

--- test/exec.c
+++ test/exec.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #define READ_SZ 10240
 
 int main(void)
@@ -12,14 +15,14 @@ int main(void)
     int                 status;
     int                 pid;
     char                *path = "site-package/index3.py";
-    
+
     // open pipe for executing cgi script
     if (pipe(input_fd) < 0 || pipe(output_fd) < 0)
     {
         perror("pipe");
         return 501;
     }
-    
+
     pid = fork();
     if (pid < 0)
     {
@@ -34,7 +37,7 @@ int main(void)
         close(output_fd[0]);
         close(input_fd[1]);
 
-        
+
         execl(path, path, NULL);
         perror("execl");
         exit(errno);
@@ -44,12 +47,12 @@ int main(void)
         // parent code
         char strbuf[READ_SZ];
         ssize_t sz;
-        
+
         // close pipe
         close(output_fd[1]);
         close(input_fd[0]);
-        
-        
+
+
         memset(strbuf, 0, READ_SZ);
         while ((sz = read(output_fd[0], strbuf, READ_SZ)) != 0)
         {
@@ -58,22 +61,22 @@ int main(void)
                 perror("read");
                 return 501;
             }
-            
+
             // write to client
             printf("OUTPUT: %s\n", strbuf);
             memset(strbuf, 0, READ_SZ);
         }
-        
+
         // wait for child to complete
         waitpid(pid, &status, 0);
-        
+
         // child has error occured
         if (WIFEXITED(status) && WEXITSTATUS(status)) {
             printf("STATUS: %d\n", WEXITSTATUS(status));
             return WEXITSTATUS(status);
         }
-        
-        
+
+
         close(output_fd[0]);
         close(input_fd[1]);
         return 0;
